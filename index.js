c3682c910f7c40ebae92f267563366c21ccab016
justin <justinmwathi32@gmail.com>
2024-03-19 23:11:12 +0300

    Functions lab continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(work="go to the office"){
+    return(`This Monday, I will ${work}.`);
+}
+
+function wrapAdjective(msg='*'){
+ return function(emphatic){
+  const wrappedAdj=`${msg}${emphatic}${msg}`;
+      return `You are ${wrappedAdj}!`  
+    };
+}
+
+
+
+
